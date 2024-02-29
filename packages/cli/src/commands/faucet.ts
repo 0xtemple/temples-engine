d901c7bc377e81b5c3163726e79a3b73d6114a65
web3olalala <w13225232270@163.com>
2024-02-29 11:29:01 +0800

    Add fauct、localnode、upload

--- packages/cli/src/commands/faucet.ts
+++ packages/cli/src/commands/faucet.ts
@@ -1,4 +1,6 @@
 import type { CommandModule } from "yargs";
+import {GearApi, GearKeyring} from '@gear-js/api';
+import {ApiPromise, WsProvider} from "@polkadot/api";
 
 type Options = {
   network: any;
@@ -27,6 +29,28 @@ const commandModule: CommandModule<Options, Options> = {
 
   async handler({ network, recipient }) {
     console.log(network, recipient)
+    let address = "";
+    if (recipient === undefined) {
+      const mnemonic = process.env.MNEMONIC;
+      if (!mnemonic)
+        throw new Error(
+            `Missing PRIVATE_KEY environment variable.
+    Run 'echo "PRIVATE_KEY=YOUR_PRIVATE_KEY" > .env'
+    in your contracts directory to use the default sui private key.`
+        );
+      const keyring = await GearKeyring.fromMnemonic(mnemonic, 'Developer');
+      address = keyring.address
+    } else {
+      address = recipient;
+    }
+
+    const wsProvider = new WsProvider("ws://localhost:9944");
+    const gearApi = await ApiPromise.create({ provider: wsProvider });
+
+    const aliceKeyring = await GearKeyring.fromSuri('//Alice');
+    const result = await gearApi.tx.balances.transfer(address, 1000e12).signAndSend(aliceKeyring);
+    console.log(address)
+    await gearApi.disconnect()
   }
 };
 
--- packages/cli/src/commands/upload.ts
+++ packages/cli/src/commands/upload.ts
@@ -2,6 +2,13 @@ import type { CommandModule } from "yargs";
 import { logError } from "../utils/errors";
 import { uploadHandler } from "../utils";
 import { loadConfig, TempleConfig } from "@0xtemple/common";
+import {
+  GearApi,
+  GearKeyring,
+  GearMetadata,
+  ProgramMetadata
+} from '@gear-js/api';
+import * as fs from "fs";
 
 type Options = {
   network: any;
@@ -29,13 +36,31 @@ const commandModule: CommandModule<Options, Options> = {
   },
 
   async handler({ network, configPath }) {
-    try {
+      const gearApi = await GearApi.create({
+        providerAddress: 'ws://localhost:9944',
+      });
       const templeConfig = (await loadConfig(configPath)) as TempleConfig;
-      await uploadHandler(templeConfig.name, network);
-    } catch (error: any) {
-      logError(error);
-      process.exit(1);
-    }
+      console.log(templeConfig.name)
+      const code = fs.readFileSync(process.cwd()+`/contracts/target/wasm32-unknown-unknown/release/${templeConfig.name}.wasm`);
+
+      const program = {
+        code,
+        gasLimit: 750000000000,
+      };
+
+      const metaHex = fs.readFileSync(process.cwd()+`/contracts/target/wasm32-unknown-unknown/release/${templeConfig.name}.meta.txt`, 'utf-8');
+    const meta = ProgramMetadata.from(`0x${metaHex}`);
+
+      const { programId, codeId, salt, extrinsic } = gearApi.program.upload(program, meta);
+    const mnemonic = process.env.MNEMONIC;
+    if (!mnemonic)
+      throw new Error(
+          `Missing PRIVATE_KEY environment variable.
+    Run 'echo "PRIVATE_KEY=YOUR_PRIVATE_KEY" > .env'
+    in your contracts directory to use the default sui private key.`
+      );
+    const keyring = await GearKeyring.fromMnemonic(mnemonic, 'Developer');
+    await extrinsic.signAndSend(keyring)
     process.exit(0);
   },
 };
