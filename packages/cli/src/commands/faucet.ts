d1768c7708e102f3fbd3218e9314d2fedd617abc
vladilen11 <vladilenaksana@163.com>
2024-03-02 16:53:50 +0800

    update cli

--- packages/cli/src/commands/faucet.ts
+++ packages/cli/src/commands/faucet.ts
@@ -1,6 +1,6 @@
 import type { CommandModule } from "yargs";
-import {GearApi, GearKeyring} from '@gear-js/api';
-import {ApiPromise, WsProvider} from "@polkadot/api";
+import { GearApi, GearKeyring } from "@gear-js/api";
+import { ApiPromise, WsProvider } from "@polkadot/api";
 
 type Options = {
   network: any;
@@ -28,18 +28,18 @@ const commandModule: CommandModule<Options, Options> = {
   },
 
   async handler({ network, recipient }) {
-    console.log(network, recipient)
+    console.log(network, recipient);
     let address = "";
     if (recipient === undefined) {
       const mnemonic = process.env.MNEMONIC;
       if (!mnemonic)
         throw new Error(
-            `Missing PRIVATE_KEY environment variable.
+          `Missing PRIVATE_KEY environment variable.
     Run 'echo "PRIVATE_KEY=YOUR_PRIVATE_KEY" > .env'
     in your contracts directory to use the default sui private key.`
         );
-      const keyring = await GearKeyring.fromMnemonic(mnemonic, 'Developer');
-      address = keyring.address
+      const keyring = await GearKeyring.fromMnemonic(mnemonic, "Developer");
+      address = keyring.address;
     } else {
       address = recipient;
     }
@@ -47,11 +47,12 @@ const commandModule: CommandModule<Options, Options> = {
     const wsProvider = new WsProvider("ws://localhost:9944");
     const gearApi = await ApiPromise.create({ provider: wsProvider });
 
-    const aliceKeyring = await GearKeyring.fromSuri('//Alice');
-    const result = await gearApi.tx.balances.transfer(address, 1000e12).signAndSend(aliceKeyring);
-    console.log(address)
-    await gearApi.disconnect()
-  }
+    const aliceKeyring = await GearKeyring.fromSuri("//Alice");
+    const result = await gearApi.tx.balances
+      .transfer(address, 1000e12)
+      .signAndSend(aliceKeyring);
+    console.log(address);
+  },
 };
 
 export default commandModule;
