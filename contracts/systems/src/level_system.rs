0e8bf26d16c0a9954193b0c2808695f876f5be21
web3olalala <w13225232270@163.com>
2024-03-04 23:55:34 +0800

    Delete contracts

--- contracts/systems/src/level_system.rs
+++
@@ -1,5 +0,0 @@
-use engine_schema::*;
-
-pub fn set_level(entity: u128, amount: u128) {
-    level::set(entity, amount);
-}
--- examples/counter/build.rs
+++
@@ -1,5 +0,0 @@
-use counter_io::CounterMetadata;
-
-fn main() {
-    gear_wasm_builder::build_with_metadata::<CounterMetadata>();
-}
